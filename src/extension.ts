40063e34896530f9721c1874afe75e0928f2f1d9
PFinal_南丞 <1370288056@qq.com>
2023-06-06 15:40:57 +0800

    2023-06-06

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pf-generated-string.pgs', () => {
+	let disposable = vscode.commands.registerCommand('pgs', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 		  const selection = editor.selection;
